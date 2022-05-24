1938f4f33da3fe505f1d8f66a6ef1aa9d355e0e6
Amkam <amaduskamara@gmail.com>
2022-05-24 07:57:47 +0000

    Add mock users array as the db

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,22 @@ import express from "express";
 
 const router = express.Router();
 
+// Mock database for users
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@email.com",
+    age: 21,
+  },
+  {
+    firstName: "Samuel",
+    lastName: "Amkam",
+    email: "john@email.com",
+    age: 78,
+  },
+];
+
 // GET /users
 router.get("/", (req, res) => {
   res.send("Hello");
